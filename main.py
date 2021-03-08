be671256298608c642feb8aede789a2c5ff4732f
W7k-J <59022102+W7k-J@users.noreply.github.com>
2021-03-09 00:02:38 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  8 22:33:42 2021
+
+@author: w7k-j
+@https://github.com/W7k-J
+
+"""
+
+def main_menu_print():
+    print("""1. Create an account\n2. Log into account\n0. Exit""")
+    
+def checker_card_number(number):
+    if number[0] != "4":
+        print("wrong number")
+    elif number [:6] != "400000":
+        print("wrong number")
+    elif len(number) != 16:
+        print("wrong number")
+    
+def checker_pin(pin):
+    try len 
+
+    
+    
+def create_account():
+    card_number = input()
+    pin = input()
+
+
+# number : (pin, money)
+accounts = {}
+
+while True:
+    
+    Main_menu_print()
+    
+    
+    break
