04469603cc1e3cee0977013f4f667c1abf2bcc5f
W7k-J <59022102+W7k-J@users.noreply.github.com>
2021-03-26 16:18:27 +0100

    Update main.py
    Stage 3

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ Created on Mon Mar  8 22:33:42 2021
 
 import sys
 import random
+import sqlite3
+
+
 
 def main_menu_print():
     if Log == False:
         print("""1. Create an account\n2. Log into account\n0. Exit""")
     else:
         print("""1. Balance\n2. Log out\n0. Exit""")
-    
+
 def checker_card_number(number):
     if number[0] != "4":
         print("wrong number")
@@ -23,9 +26,9 @@ def checker_card_number(number):
         print("wrong number")
     elif len(number) != 16:
         print("wrong number")
-    
+
 # def checker_pin(pin):
-#     try len 
+#     try len
 
 def log_in():
     card_number = input("Enter your card number \n")
@@ -36,10 +39,11 @@ def log_in():
         Log = True
         global current_log
         current_log = card_number
+
         print("You have successfully logged in!")
     else:
         print("Wrong card number or PIN!")
-        
+
 def log_out():
     global Log
     Log = False
@@ -49,35 +53,35 @@ def log_out():
 def balance_check():
     print("\n")
     print(f"Balance: {database[current_log][1]}")
-    print("\n")    
-    
+    print("\n")
+
 def create_account():
     print("Your card has been created")
-    
-    
+
+
     print("Your card number:")
     while True:
-        #control sum
+
+        #random card without last number
         temp_cn = ("400000" + ''.join([str(random.randrange(10)) for k in range(9)]))
+
+        #control sum
         total = 0
         for num, char in enumerate(temp_cn):
-            print(str(num) + "----" + str(char))
             if (num + 1)% 2 == 1:
                 mult = int(char) * 2
-                if mult > 9:  
-                    total = total + mult - 9 
+                if mult > 9:
+                    total = total + mult - 9
                 else:
                     total = total + mult
             else:
                 total = total + int(char)
-        print(total)
+
         last_digit = str(10 - (total % 10))
 
-        print(last_digit)
-        
-        temp_card_number = temp_cn + last_digit[-1]        
-         
-        #check if in database
+        temp_card_number = temp_cn + last_digit[-1]
+
+        #check if in database - new random if it is there! break if number unused
         if temp_card_number not in database:
             card_number = temp_card_number
             break
@@ -89,8 +93,39 @@ def create_account():
 
     database[card_number] = [pin, 0]
 
+    cur.execute(f"""INSERT INTO card (
+        number,
+        pin
+        )
+        VALUES
+        (
+        {card_number},
+        {pin}
+        );
+        """)
+    conn.commit()
+
 # database[4000004938320895][1]  = 1123
 
+
+
+
+
+try:
+    conn = sqlite3.connect('card.s3db')
+    cur = conn.cursor()
+
+    cur.execute("""CREATE TABLE card (
+            id INTEGER NOT NULL AUTO_INCREMENT,
+            number TEXT,
+            pin TEXT,
+            balance INTEGER DEFAULT 0
+    );
+            """)
+
+    conn.commit()
+except:
+    print()
 
 # number : (pin, money)
 database = {}
